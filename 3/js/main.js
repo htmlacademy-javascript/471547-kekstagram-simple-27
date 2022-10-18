d9afcc5705d1f68b8ab78db85cf39f754d99c9c5
Keks <keks@htmlacademy.ru>
2022-10-18 21:55:10 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,15 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+function randomInteger(min, max) {
+    // случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+console.log(randomInteger(1, 10));
+
+
+function checkStringLength(currentStr, maxLength) {
+    return (currentStr.length <= maxLength) ? true : false;
+}
+console.log(checkStringLength('Проверочный текст', 3));
