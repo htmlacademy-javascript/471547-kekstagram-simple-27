ac5b140d2ea234c1a4c0da693d22f9a2b0c39924
Katerina Baidukova <95221323+kate-baidukova@users.noreply.github.com>
2022-10-28 12:10:12 +0300

    Изменяет отступы

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
 function randomInteger(min, max) {
-    const rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
 }
 (randomInteger(1, 10));
 
 
 function checkStringLength(currentStr, maxLength) {
-    return (currentStr.length <= maxLength);
+  return (currentStr.length <= maxLength);
 }
 (checkStringLength('Проверочный текст', 33));
